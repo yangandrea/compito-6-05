51b19b6094b3dc4fea30d48dcf4f1b7b4ea10167
lai2-24 <lai2-24@itismeucci.com>
2024-05-06 10:42:37 +0200

    terzo

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   const [game, setGame] = useState(null);
-  const [Indovina, impostaIndovina] = useState('');
-  const [caricamento, setLoading] = useState(false);
-  const [risultato, setResult] = useState(null);
+  const [guess, setGuess] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState(null);
 
-  const inzianuovogioco = () => {
+  const startNewGame = () => {
     setLoading(true);
+    setGuess('');
+    setResult(null);
     fetch('http://localhost:8080/partita', {
       method: 'POST',
       headers: {
@@ -20,49 +22,47 @@ function App() {
       setGame(data);
       setLoading(false);
     })
-    .catch((error) => {
-      console.error('Error:', error);
-      setLoading(false);
-    });
   };
 
-  const ilnumero = () => {
+  const submitGuess = () => {
     setLoading(true);
     fetch(`http://localhost:8080/partita/${game.id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ numero: Indovina }),
+      body: JSON.stringify({ numero: guess }),
     })
     .then(response => response.json())
     .then(data => {
       setResult(data);
+      setGame(prevGame => ({ ...prevGame, tentativi: data.tentativi }));
       setLoading(false);
     })
-    .catch((error) => {
-      console.error('Error:', error);
-      setLoading(false);
-    });
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Indovina il numero</h1>
-        <button onClick={inzianuovogioco}>Nuova partita</button>
-        <hr/>
-        {
-          caricamento ? 
-            <div>In caricamento...</div>
+        <button onClick={startNewGame}>Nuova partita</button>
+        <hr />
+        { 
+          loading ? 
+            <div>In caricamento... </div>
           :
             game ? (
               <div>
                 <p>Game ID: {game.id}</p>
-                <p>Inserisci un numero tra 1 e 100</p>
-                <input type="text" value={Indovina} onChange={e => impostaIndovina(e.target.value)} />
-                <button onClick={ilnumero}>Invia</button>
-                {risultato && <p>Risultato: {risultato.risultato}, Tentativi: {risultato.tentativi}</p>}
+                {result && result.risultato === 0 ? (
+                  <p>Hai indovinato il numero!</p>
+                ) : (
+                  <>
+                    <input type="number" min="1" max="100" value={guess} onChange={e => setGuess(e.target.value)} />
+                    <button onClick={submitGuess}>Invia</button>
+                  </>
+                )}
+                {result && <p>Risultato: {result.risultato}, Tentativi: {game.tentativi}</p>}
               </div>
             ) : null
         }
